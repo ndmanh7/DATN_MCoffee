66a874c27b88ce20fb432e245c15c8cfe4150a3f
manh <ndmanh10a7@gmail.com>
2023-03-15 20:35:17 +0700

    add login screen

---
+++ app/src/main/java/com/example/mcoffee/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mcoffee.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mcoffee.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
